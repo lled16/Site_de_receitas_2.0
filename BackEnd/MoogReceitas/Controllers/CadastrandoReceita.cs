ae02e21a5b776f1841a128a52c40a20c48e7ff5e
lled16 <vitoralves1604@gmail.com>
2022-11-25 10:46:58 -0300

    Backend

--- BackEnd/MoogReceitas/Controllers/CadastrandoReceita.cs
+++ BackEnd/MoogReceitas/Controllers/CadastrandoReceita.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoogReceitas.DataBase;
+using MoogReceitas.Models;
 using System.Data;
 using System.Web.Http.Cors;
 
@@ -14,7 +17,23 @@ namespace MoogReceitas.Controllers
         public string CadReceita(string name, int porcoes, int tmpPreparo, string categoria, string ingredientes, string modoPreparo)
         {
 
-          
+            DataContext cadReceita = new DataContext();
+            Random randNum = new Random();
+
+
+            var receita = new CadastroReceita()
+            {
+                Id = randNum.Next(),
+                Titulo = name,
+                Categoria = categoria,
+                TempoPreparo = tmpPreparo,
+                Rendimento = porcoes,
+                Ingredientes = ingredientes,
+                ModoPreparo = modoPreparo
+            };
+
+            cadReceita.receitas.Add(receita);
+            cadReceita.SaveChanges();
             return "Receita cadastrada !";
         }
     }
--- BackEnd/MoogReceitas/DataBase/DataContext.cs
+++ BackEnd/MoogReceitas/DataBase/DataContext.cs
@@ -5,12 +5,12 @@ namespace MoogReceitas.DataBase
 {
     public class DataContext : DbContext
     {
-        public virtual DbSet<CadastroReceita> receita { get; set; }
-        public virtual DbSet<Login> login { get; set; }
+        public virtual DbSet<CadastroReceita> receitas { get; set; }
+    
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 
-            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB;Initial Catalog=eleicao;Integrated Security=True;Pooling=False;MultipleActiveResultSets=True");
+            optionsBuilder.UseSqlServer("Data Source = mysql://root:d0NQIB7WNqJ3CMBWnMEP@containers-us-west-84.railway.app:7896/railway;Initial Catalog=railway;Integrated Security=True;Pooling=False;MultipleActiveResultSets=True");
         }
     }
 }
